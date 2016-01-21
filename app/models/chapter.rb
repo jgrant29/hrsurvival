11c0919e917553e526e8cca3d85db88c7f64b664
Justin Grant <jmgrant702@gmail.com>
2016-01-21 12:08:44 -0700

    default scope for chapter and toc

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,7 @@
 class Chapter < ActiveRecord::Base
   belongs_to :book
   has_many :tableofcontents
+
+    default_scope { order('title ASC') }
+
 end
--- app/models/tableofcontent.rb
+++ app/models/tableofcontent.rb
@@ -2,4 +2,6 @@ class Tableofcontent < ActiveRecord::Base
   belongs_to :chapter  
   belongs_to :book
 
+
+  default_scope { order('title ASC') }
 end
