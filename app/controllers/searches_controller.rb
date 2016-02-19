ec7e3e305b4ecaeddefc881b8d43cc1b30462747
Justin Grant <jmgrant702@gmail.com>
2016-02-19 12:48:26 -0700

    search controller fix and ad placement

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,10 @@
 class SearchesController < ApplicationController
   def index
     @tableofcontent_searches = Tableofcontent.search(params[:query], operator: "or", fields: [:title, :body], page: params[:page], per_page: 12, highlight: {tag: "<strong>", fields: {body: {fragment_size: 245, fragment: 3}}})
-    @chapter_searches = Chapter.search(params[:query], operator: "or", fields: [:title])
-
   end
 
   def autocomplete
     tableofcontent = Tableofcontent.search(params[:term], limit: 5).map(&:title)
-    chapter = Chapter.search(params[:term], limit: 5).map(&:title)
     render json: (tableofcontent)
   end
 end
