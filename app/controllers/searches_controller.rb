ff9a6d6f6d91a6bbb227f0afd1db9913be897229
Justin Grant <jmgrant702@gmail.com>
2016-02-22 10:05:06 -0700

    search index bug

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def index
-    @tableofcontent_searches = Tableofcontent.search(params[:query], operator: "or", fields: [:title, :body], page: params[:page], per_page: 12, highlight: {tag: "<strong>", fields: {body: {fragment_size: 245}}})
+    @tableofcontent_searches = Tableofcontent.search(params[:query], operator: "or", fields: [:body], page: params[:page], per_page: 12, highlight: {tag: "<strong>", fields: {body: {fragment_size: 245, number_of_fragments: 3}}})
   end
 
   def autocomplete
