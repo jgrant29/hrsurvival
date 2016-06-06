175d72deba780ddba6d7bb1b75d71f441566fcd4
Justin Grant <jmgrant702@gmail.com>
2016-06-06 14:25:08 -0600

    image file type ckeditor

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -35,4 +35,9 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   def extension_white_list
     Ckeditor.attachment_file_types
   end
+
+  def extension_white_list
+    %w(jpg jpeg gif png pdf)
+  end
+
 end
