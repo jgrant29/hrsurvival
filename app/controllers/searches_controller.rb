74cabf046874d7e6af82f27495785e4f1e99071b
Justin Grant <jmgrant702@gmail.com>
2016-09-08 13:51:35 -0600

    search if empty text

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def index
-    @tableofcontent_searches = Tableofcontent.search(params[:query], operator: "or", fields: [:body, :title],  page: params[:page], per_page: 12, highlight: {tag: "<strong>", fields: {body: {fragment_size: 245, number_of_fragments: 3}}}, page: params[:page], per_page: 3)
+    @tableofcontent_searches = Tableofcontent.search(params[:query], operator: "or", fields: [:body, :title],  page: params[:page], per_page: 12, highlight: {tag: "<strong>"}, page: params[:page], per_page: 3)
     @article_searches = Article.search(params[:query], operator: "or", fields: [:body, :title], page: params[:page], per_page: 12, highlight: {tag: "<strong>", fields: {body: {fragment_size: 245, number_of_fragments: 3}}}, page: params[:page], per_page: 3)
     @results = (@tableofcontent_searches && @article_searches)
   end
